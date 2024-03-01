524a5b3f52253defa9ea604a38383a093a6e8e60
Iannn <pristianb007@gmail.com>
2024-03-01 19:49:37 +0700

    TS Introduction

---
+++ 01. Introduction/src/01. Introduction.ts
@@ -0,0 +1,36 @@
+let username = `Carl Gauss`;
+console.log(`Hello, ${username}!`);
+
+// Sum of an array
+function sum(array: number[]) {
+  return array.reduce(
+    (accumulator, currentValue) => accumulator + currentValue
+  );
+}
+
+console.log(sum([1, 2, 3, 4, 5]));
+
+/*
+A valid JS is also a valid TS. However, it doesn't mean that the TS compiler will like something like below.
+
+Remember that one meme? The one says 2 + "2" = 22, while 2 - "2" = 0
+
+However, this issue can be resolved by using "use strict" in JS, while in TS we have to uncomment "noEmitOnError": true, in tsconfig.json
+*/
+
+// You can declare your variable just as follows
+let a = 2;
+let b = "2";
+
+// Or
+let x: number = 2;
+let y: string = "2";
+
+let basicMath = [
+  { Operator: "+", Value: a + Number(b) },
+  { Operator: "-", Value: a - Number(b) },
+  { Operator: "*", Value: a * Number(b) },
+  { Operator: "/", Value: a / Number(b) },
+];
+
+console.table(basicMath);
